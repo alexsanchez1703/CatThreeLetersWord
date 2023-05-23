b7eee793cdf4e59fa6b1c2a9114032da80e53d6d
Jorge Sanchez <jasr0096@gmail.com>
2023-05-22 22:01:01 -0500

    Prueba tecnica 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const ENDPOINT_RANDOM_FACTS = "https://catfact.ninja/fact"
+//const ENDPOINT_CATS_IMG = "https://cataas.com/cat/says/${firstWord}?size=50&color=red"
+const ENDPOINT_PREFIXE_URL = "https://cataas.com"
+
 
 function App() {
+
+  const [fact, setFact] = useState()
+  const [imgUrl, setImgUrl] = useState()
+
+  useEffect(() => {
+    fetch(ENDPOINT_RANDOM_FACTS)
+      .then(resp => resp.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+        const trheeFirstWord = fact.split(' ', 3).join(' ')
+        console.log(trheeFirstWord)
+        fetch(`https://cataas.com/cat/says/${trheeFirstWord}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImgUrl(url)
+          })
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mundo de Gatos</h1>
+      {fact && <p>{fact}</p>}
+      {imgUrl && <img src= {`${ENDPOINT_PREFIXE_URL}${imgUrl} `} alt={`Imagen traida de una api para ser usada por su tres pirmeras palabras`}/> }
     </div>
   );
 }
